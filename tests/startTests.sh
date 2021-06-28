bb186c32976a444501782d93996e598d5bb5e1a9
Robert Auer <robert.auer@cloudogu.com>
2021-06-28 14:34:28 +0200

    Make test host configurable

--- tests/startTests.sh
+++ tests/startTests.sh
@@ -5,6 +5,7 @@ set -o pipefail
 
 REPOSITORY_NAME=testRepo
 PACKAGE=ces-commons_0.4.0.deb
+HOST=localhost
 
 #
 # ----------------------------------------------------------------------------------------------------------------------
@@ -14,7 +15,7 @@ echo "[Test] Create a repository..."
 
 RESPONSE=$(curl -s -o /dev/null -w "%{http_code}" \
   --request POST \
-  --url http://192.168.56.2:8080/api/repos \
+  --url http://${HOST}:8080/api/repos \
   --header 'Content-Type: application/json' \
   --data "{\"Name\": \"${REPOSITORY_NAME}\"}")
 
@@ -35,7 +36,7 @@ RESPONSE=$(curl -s -o /dev/null -w "%{http_code}" \
   --request DELETE \
   --header 'Content-Type: application/json' \
   --data "{\"Name\": \"${REPOSITORY_NAME}\"}" \
-  --url http://192.168.56.2:8080/api/repos/"${REPOSITORY_NAME}")
+  --url http://${HOST}:8080/api/repos/"${REPOSITORY_NAME}")
 
 if [[ ${RESPONSE} != 200 ]]; then
   echo "[Test] Delete a repository - FAILED! - Error code should be 200 but got ${RESPONSE}"
@@ -53,7 +54,7 @@ echo "[Test] Upload a package..."
 RESPONSE=$(curl -s -o /dev/null -w "%{http_code}" \
   --request POST \
   -F file="${PACKAGE}" \
-  --url http://192.168.56.2:8080/api/files/"${PACKAGE}")
+  --url http://${HOST}:8080/api/files/"${PACKAGE}")
 
 if [[ ${RESPONSE} != 200 ]]; then
   echo "[Test] Upload a package - FAILED! - Error code should be 200 but got ${RESPONSE}"
